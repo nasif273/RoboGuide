9fd54acc00e133171c08bd25507b5d12efbd0e7a
nasif273 <abdullahomarn57@gmail.com>
2024-02-18 03:07:50 +0600

    nasif 4

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import ChatBot from 'ChatBot.jsx'
+import ChatBot from './ChatBot.jsx'
 import './App.css'
 
 function App() {
