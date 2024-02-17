be1766c9d05a71d6c998f991a25b5929f01be122
sabbir-063 <202114063@student.mist.ac.bd>
2024-02-17 18:37:32 +0600

    sabbir

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import ChatBot from './components/ChatBot.jsx'
+import ChatBot from './ChatBot.jsx'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <div>
       <ChatBot />
-    </>
+    </div>
   )
 }
 
